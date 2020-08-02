54c5917226edeed4070c9baea1229c375faabb72
Keylla <keylla03@gmail.com>
2020-08-02 20:36:43 -0300

    commit final da aula 4

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,11 @@
-import styled from "styled-components";
+/* eslint-disable linebreak-style */
+import styled from 'styled-components';
 
 const Button = styled.button`
     color: var(--white);
     border: 1px solid var(--white);
     box-sizing: border-box;
+    background: var(--black);
     cursor: pointer;
     padding: 16px 24px;
     font-style: normal;
